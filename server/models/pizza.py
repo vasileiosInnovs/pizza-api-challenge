175914fa65bab2a2b2af64a1d2d4ed9b6b9f9f9e
Hagee <hagee.ktr@outlook.com>
2025-06-13 14:54:51 +0300

    Generate pizza class

---
+++ server/models/pizza.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    ingredients = db.Column(db.String())
+
+    RestaurantPizza = db.relationship('restaurantpizza', back_populates="pizza", cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<{self.id}, {self.name}, {self.ingredients}'
