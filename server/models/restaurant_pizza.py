51389721ba29056fa4e323daf4a79753b6f09ccc
Hagee <hagee.ktr@outlook.com>
2025-06-16 21:19:11 +0300

    [Feature]: generate validations for validation

--- server/models/restaurant_pizza.py
+++ server/models/restaurant_pizza.py
@@ -1,15 +1,25 @@
+from sqlalchemy.orm import validates
+from sqlalchemy import CheckConstraint
 from . import db
 
 class RestaurantPizza(db.Model):
-    __tablename__ = "restaurantpizzas"
+    __tablename__ = "restaurant_pizzas"
 
     id = db.Column(db.Integer(), primary_key=True)
     price = db.Column(db.Integer())
     restaurant_id = db.Column(db.Integer(), db.ForeignKey('restaurants.id'))
     pizza_id = db.Column(db.Integer(), db.ForeignKey('pizzas.id'))
 
-    pizza = db.relationship('Pizza', back_populates="restaurantpizza")
-    restaurant = db.relationship('Restaurant', back_populates="restaurantpizza")
+    pizza = db.relationship('Pizza', back_populates="restaurant_pizza")
+    restaurant = db.relationship('Restaurant', back_populates="restaurant_pizza")
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError("Price must be between 1 and 30")
+        return value
+    
+    __table_args__ = (CheckConstraint('price >= 1 AND price <=30', name='check_price_range'))
 
     def __repr__(self):
         return f'<{self.id}, Price:{self.price}, Restaurant:{self.restaurant_id}, Pizza:{self.pizza_id}>'
